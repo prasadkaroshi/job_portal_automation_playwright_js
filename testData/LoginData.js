d418e5214b399a0fdbc3dc9dfb16a8fd9ab8eb7e
Prasad Karoshi <prasadkaroshi@MacBookAir.local>
2026-07-14 19:02:37 +0530

    pdf added env

--- testData/LoginData.js
+++ testData/LoginData.js
@@ -2,6 +2,6 @@ export const loginData = {
     url:process.env.URL,
     username:process.env.APP_USERNAME,
     password:process.env.APP_PASSWORD,
-    filePath:"testData/Ramprasad_Karoshi_QA_Automation_Engineer_Resume.pdf",
-    filePath1:"testData/Ramprasad_Karoshi_QA_Automation_Performance_Engineer_Resume.pdf"
+    filePath:process.env.RESUME_PATH_AUTOMATION,
+    filePath1:process.env.RESUME_PATH_PERFORMANCE
 }
